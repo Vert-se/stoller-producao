f650b60ac6c9ddf12e5ba7de169c58fa53e86bae
Thiago Dutra <taodutra@gmail.com>
2015-11-24 16:03:19 -0200

    A Stoller - Finalizada

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,22 +7,35 @@ site.init = function(){
     this.w = $(window);
     this.w_w = this.w.width();
     this.w_h = this.w.height();
+    this.resp_w = 850;
 
     this.b = $('body').eq(0);
     this.b_h = this.b.height();
+
 
     // this.h = $('header');
     // this.h_h = this.h.outerHeight() + 1; // 1 de border do pai
 
     // this.loading = $('#loading');
 
+    this.areas_el = $('.area');
+    this.areas_y = [];
+    this.areas_active = 0; 
+    this.areas_old = '';
+    this.areas_pos();
+
     this.resize();
 
-    this.link_set();
 
-    // this.mask_form();
+    this.on_scroll_action();
 
-    // this.clear_form_errors();
+    this.on_scroll();
+
+    if( $('.area_nav').length > 0 && (this.w_w >= this.resp_w)){
+        this.area_nav();
+    }else{
+        this.link_set();
+    }
 
     $('.grid_gallery').slick({
       dots: false,
@@ -44,6 +57,74 @@ site.init = function(){
 
 };
 
+site.areas_pos = function(){
+    var that = this, c = 0;
+
+    this.areas_el.each(function(){
+        var t = $(this), y = t.position().top;
+        that.areas_y[c] = y;
+        c++;
+    });
+
+    // console.log( this.areas_y );
+}
+
+site.area_nav = function(){
+    var that = this, c = 0, an = $('.area_nav'), an_box = '<nav class="area_nav_box"><ul>';
+
+    an.each(function(){
+        c++;
+        var t = $(this), target = t.attr('id'), title = t.data('title').replace(' ', '&nbsp;'), tc = (c==1) ? 'active' : '';
+
+        an_box += '<li class="'+tc+'"><a href="" rel="scroll" data-target="'+target+'" id="an_'+target+'">';
+        an_box += '<span>'+title+'</span>';
+        an_box += '</a></li>';
+
+    });
+
+    an_box += '</ul></nav>';
+
+    this.b.append(an_box);
+    this.link_set();
+
+};
+
+site.areas_check = function(element, index, array) {
+    var that = window.site;
+
+    if((that.w_s + 100) >= element){
+        that.areas_active = index;
+    }
+}
+
+site.on_scroll = function(){
+    var that = this;
+
+
+    this.w.scroll(function() {
+
+        that.w_s = that.w.scrollTop();
+
+        that.areas_y.forEach(that.areas_check);
+
+        that.on_scroll_action();
+
+    });
+
+
+}
+
+site.on_scroll_action = function(){
+    var that = this, m = $('.area_nav_box ul');
+
+    if(this.areas_active != this.areas_old){
+
+        this.areas_old = this.areas_active;
+        m.find('.active').removeClass('active');
+        m.find('li').eq(this.areas_active).addClass('active');
+    }
+}
+
 site.resize = function(){
     var that = this;
 
@@ -53,6 +134,8 @@ site.resize = function(){
         that.w_h = that.w.height();
 
         that.b_h = that.b.height();
+
+        that.areas_pos();
 
     });
 };
@@ -83,7 +166,7 @@ site.link_set = function(){
         l_scroll.on('click',function(event){
             event.preventDefault();
 
-            var t = $(this), target_pos = (t.data('target')) ? $('#'+t.data('target')).position().top - that.h_h : 0;
+            var t = $(this), target_pos = (t.data('target')) ? $('#'+t.data('target')).position().top : 0;
 
             $('html, body').stop().animate({
                 scrollTop: target_pos
