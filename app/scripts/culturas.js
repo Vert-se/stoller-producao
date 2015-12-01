03bbd283ed55fbf1ad6206e3700048b96b6da3f4
Thiago Dutra <taodutra@gmail.com>
2015-12-01 09:28:20 -0200

    Estrutura base de Culturas e Galeria 1 de Soja

---
+++ app/scripts/culturas.js
@@ -0,0 +1,19 @@
+window.Culturas = {
+    baseUrl: 'images/culturas/',
+    pages: {
+        soja: [
+            {
+                type: 'animacao',
+                steps: [
+                    { html: '<p class="sub-title"><strong>até 40 dias após emergência</strong></p>' },
+                    { html: '<p class="sub-title"><strong>até 40 dias após emergência</strong></p>' },
+                    { html: '<p class="sub-title"><strong>60 dias após emergência</strong></p>' },
+                    { html: '<p class="sub-title"><strong>de 60 a 90 dias após emergência</strong></p>' },
+                    { html: '<p class="sub-title"><strong>lorem ipsum</strong></p>' },
+                    { html: '<p class="sub-title"><strong>lorem ipsum</strong></p>' },
+                    { html: '<p class="sub-title"><strong>Sistema Integrado Stoller (SIS)</strong><br />O Sistema Integrado Stoller  é uma solução completa com produtos específicos para cada fase do ciclo de vida do algodão. A aplicação do SIS contribui para a  proteção e para o fortalecimento das plantas.</p><p><a href="especialista-stoller.html">Procure o Especialista<br />Stoller mais próximo</a></p>' },
+                ]
+            }
+        ]
+    }
+}
