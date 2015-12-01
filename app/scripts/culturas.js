a346b36e1d49bf5da515e80a342ab106e011e614
Thiago Dutra <taodutra@gmail.com>
2015-12-01 13:32:48 -0200

    Galeria 1 - desktop

--- app/scripts/culturas.js
+++ app/scripts/culturas.js
@@ -1,6 +1,113 @@
 window.Culturas = {
     baseUrl: 'images/culturas/',
     pages: {
+        algodao: [
+            {
+                type: 'animacao',
+                steps: [
+                    { html: '<p class="sub-title"><strong>até 40 dias após emergência</strong></p>' },
+                    { html: '<p class="sub-title"><strong>até 40 dias após emergência</strong></p>' },
+                    { html: '<p class="sub-title"><strong>60 dias após emergência</strong></p>' },
+                    { html: '<p class="sub-title"><strong>de 60 a 90 dias após emergência</strong></p>' },
+                    { html: '<p class="sub-title"><strong>de 90 a 120 dias após emergência</strong></p>' },
+                    { html: '<p class="sub-title"><strong>lorem ipsum</strong></p>' },
+                    { html: '<p class="sub-title"><strong>Sistema Integrado Stoller (SIS)</strong><br />O Sistema Integrado Stoller  é uma solução completa com produtos específicos para cada fase do ciclo de vida do algodão. A aplicação do SIS contribui para a  proteção e para o fortalecimento das plantas.</p><p><a href="especialista-stoller.html">Procure o Especialista<br />Stoller mais próximo</a></p>' },
+                ]
+            }
+        ],
+        arroz: [
+            {
+                type: 'animacao',
+                steps: [
+                    { html: '<p class="sub-title"><strong>SEMENTES</strong></p>' },
+                    { html: '<p class="sub-title"><strong>PERFILHAMENTO</strong></p>' },
+                    { html: '<p class="sub-title"><strong>EMBORRACHAMENTO</strong></p>' },
+                    { html: '<p class="sub-title"><strong>INÍCIO DA EMISSÃO DA PANÍCULA</strong></p>' },
+                    { html: '<p class="sub-title"><strong>Sistema Integrado Stoller (SIS)</strong><br />O Sistema Integrado Stoller  é uma solução completa com produtos específicos para cada fase do ciclo de vida do arroz. A aplicação do SIS contribui para a  proteção e para o fortalecimento das plantas.</p><p><a href="especialista-stoller.html">Procure o Especialista<br />Stoller mais próximo</a></p>' },
+                ]
+            }
+        ],
+        batata: [
+            {
+                type: 'animacao',
+                steps: [
+                    { html: '<p class="sub-title"><strong>EMERGÊNCIA</strong></p>' },
+                    { html: '<p class="sub-title"><strong>EMERGÊNCIA</strong></p>' },
+                    { html: '<p class="sub-title"><strong>PERÍODO VEGETATIVO</strong></p>' },
+                    { html: '<p class="sub-title"><strong>INÍCIO DA TUBÊRIZAÇÃO</strong></p>' },
+                    { html: '<p class="sub-title"><strong>ENCHIMENTO DE TUBÉRCULOS</strong></p>' },
+                    { html: '<p class="sub-title"><strong>SENESCÊNCIA</strong></p>' },
+                    { html: '<p class="sub-title"><strong>Sistema Integrado Stoller (SIS)</strong><br />O Sistema Integrado Stoller  é uma solução completa com produtos específicos para cada fase do ciclo de vida da batata. A aplicação do SIS contribui para a  proteção e para o fortalecimento das plantas.</p><p><a href="especialista-stoller.html">Procure o Especialista<br />Stoller mais próximo</a></p>' },
+                ]
+            }
+        ],
+        cana: [
+            {
+                type: 'animacao',
+                steps: [
+                    { html: '<p class="sub-title"><strong>CANA PLANTA - PLANTIO DA MUDA</strong></p>' },
+                    { html: '<p class="sub-title"><strong>Brotação e emergência</strong></p>' },
+                    { html: '<p class="sub-title"><strong>início do perfilhamento</strong></p>' },
+                    { html: '<p class="sub-title"><strong>cana soca - formação da touceira</strong></p>' },
+                    { html: '<p class="sub-title"><strong>crescimento dos colmos</strong></p>' },
+                    { html: '<p class="sub-title"><strong>definição da população final de colmos</strong></p>' },
+                    { html: '<p class="sub-title"><strong>cana planta/cana soca - maturação dos colmos</strong></p>' },
+                    { html: '<p class="sub-title"><strong>Sistema Integrado Stoller (SIS)</strong><br />O Sistema Integrado Stoller  é uma solução completa com produtos específicos para cada fase do ciclo de vida do milho. A aplicação do SIS contribui para a  proteção e para o fortalecimento das plantas.</p><p><a href="especialista-stoller.html">Procure o Especialista<br />Stoller mais próximo</a></p>' },
+                ]
+            }
+        ],
+        cafe: [
+            {
+                type: 'animacao',
+                steps: [
+                    { html: '<p class="sub-title"><strong>pré florada</strong></p>' },
+                    { html: '<p class="sub-title"><strong>queda das pétalas</strong></p>' },
+                    { html: '<p class="sub-title"><strong>chumbinho</strong></p>' },
+                    { html: '<p class="sub-title"><strong>expansão dos frutos / granação</strong></p>' },
+                    { html: '<p class="sub-title"><strong>início da maturação</strong></p>' },
+                    { html: '<p class="sub-title"><strong>Sistema Integrado Stoller (SIS)</strong><br />O Sistema Integrado Stoller  é uma solução completa com produtos específicos para cada fase do ciclo de vida do café. A aplicação do SIS contribui para a  proteção e para o fortalecimento das plantas.</p><p><a href="especialista-stoller.html">Procure o Especialista<br />Stoller mais próximo</a></p>' },
+                ]
+            }
+        ],
+        citros: [
+            {
+                type: 'animacao',
+                steps: [
+                    { html: '<p class="sub-title"><strong>plantas jovens</strong></p>' },
+                    { html: '<p class="sub-title"><strong>fase vegetativa</strong></p>' },
+                    { html: '<p class="sub-title"><strong>florada</strong></p>' },
+                    { html: '<p class="sub-title"><strong>pós florada</strong></p>' },
+                    { html: '<p class="sub-title"><strong>enchimento dos frutos</strong></p>' },
+                    { html: '<p class="sub-title"><strong>Fase final de maturação</strong></p>' },
+                    { html: '<p class="sub-title"><strong>Sistema Integrado Stoller (SIS)</strong><br />O Sistema Integrado Stoller  é uma solução completa com produtos específicos para cada fase do ciclo de vida do citros. A aplicação do SIS contribui para a  proteção e para o fortalecimento das plantas.</p><p><a href="especialista-stoller.html">Procure o Especialista<br />Stoller mais próximo</a></p>' },
+                ]
+            }
+        ],
+        feijao: [
+            {
+                type: 'animacao',
+                steps: [
+                    { html: '<p class="sub-title"><strong>SEMENTES</strong></p>' },
+                    { html: '<p class="sub-title"><strong>V4</strong></p>' },
+                    { html: '<p class="sub-title"><strong>v4/v6</strong></p>' },
+                    { html: '<p class="sub-title"><strong>R5</strong></p>' },
+                    { html: '<p class="sub-title"><strong>r7/r8</strong></p>' },
+                    { html: '<p class="sub-title"><strong>Sistema Integrado Stoller (SIS)</strong><br />O Sistema Integrado Stoller  é uma solução completa com produtos específicos para cada fase do ciclo de vida do feijão. A aplicação do SIS contribui para a  proteção e para o fortalecimento das plantas.</p><p><a href="especialista-stoller.html">Procure o Especialista<br />Stoller mais próximo</a></p>' },
+                ]
+            }
+        ],
+        milho: [
+            {
+                type: 'animacao',
+                steps: [
+                    { html: '<p class="sub-title"><strong>SEMENTES</strong></p>' },
+                    { html: '<p class="sub-title"><strong>V4</strong></p>' },
+                    { html: '<p class="sub-title"><strong>V8</strong></p>' },
+                    { html: '<p class="sub-title"><strong>Vt</strong></p>' },
+                    { html: '<p class="sub-title"><strong>Sistema Integrado Stoller (SIS)</strong><br />O Sistema Integrado Stoller  é uma solução completa com produtos específicos para cada fase do ciclo de vida do milho. A aplicação do SIS contribui para a  proteção e para o fortalecimento das plantas.</p><p><a href="especialista-stoller.html">Procure o Especialista<br />Stoller mais próximo</a></p>' },
+                ]
+            }
+        ],
         soja: [
             {
                 type: 'animacao',
