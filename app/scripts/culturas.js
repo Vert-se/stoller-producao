dcfd593a9da18c8df4715f4ac218c5744db97f35
Thiago Dutra <taodutra@gmail.com>
2015-12-01 10:15:32 -0200

    Galeria 1 de Tomate

--- app/scripts/culturas.js
+++ app/scripts/culturas.js
@@ -14,6 +14,19 @@ window.Culturas = {
                     { html: '<p class="sub-title"><strong>Sistema Integrado Stoller (SIS)</strong><br />O Sistema Integrado Stoller  é uma solução completa com produtos específicos para cada fase do ciclo de vida do algodão. A aplicação do SIS contribui para a  proteção e para o fortalecimento das plantas.</p><p><a href="especialista-stoller.html">Procure o Especialista<br />Stoller mais próximo</a></p>' },
                 ]
             }
+        ],
+        tomate: [
+            {
+                type: 'animacao',
+                steps: [
+                    { html: '<p class="sub-title"><strong>TRANSPLANTE</strong></p>' },
+                    { html: '<p class="sub-title"><strong>15 dias</strong></p>' },
+                    { html: '<p class="sub-title"><strong>30 dias</strong></p>' },
+                    { html: '<p class="sub-title"><strong>45 dias</strong></p>' },
+                    { html: '<p class="sub-title"><strong>60 dias</strong></p>' },
+                    { html: '<p class="sub-title"><strong>Sistema Integrado Stoller (SIS)</strong><br />O Sistema Integrado Stoller  é uma solução completa com produtos específicos para cada fase do ciclo de vida do tomate. A aplicação do SIS contribui para a  proteção e para o fortalecimento das plantas.</p><p><a href="especialista-stoller.html">Procure o Especialista<br />Stoller mais próximo</a></p>' },
+                ]
+            }
         ]
     }
 }
